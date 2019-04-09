897234d5c0ac2b30cd5c818d734c92828da08fa9
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-04-09 11:22:27 -0500

    semi working

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,45 @@
 'use strict';
 
+
+class TriviaAPI {
+  constructor(){
+  }
+
+  errorHandler(response) {
+    let error;
+    if (!response.ok) {
+      error.status = response.status;
+      if (response.headers.get('Content-Type').includes('json')) {
+        error.message = response.statusText;
+        return Promise.reject(error);
+      }
+    }
+    if (error) {
+      error.message = response.message;
+      return Promise.reject(error);
+    }
+    return response.json();
+  }
+
+  static getQuestions() {
+
+    const BASE_URL = 'https://opentdb.com/api.php?amount=5';
+  
+    return fetch(`${BASE_URL}`)
+      .then(this.errorHandler)
+      .then(res => res.json());
+  
+    //.then(this.errorHandler)
+    // .then(res => res.results.forEach(question => {
+    //allQuestions.push( new Question(question.question, question.incorrect_answers, question.correct_answer));
+    // }))
+    // .catch(e => console.log(e.message));
+    // }
+    
+  }
+
+}
+
 class Quiz{
   constructor(){
     this.unasked = [];
@@ -7,7 +47,7 @@ class Quiz{
     this.score = 0;
     this.scoreHistory = [];
     this.active = false;
-    this.checkValue = null
+    this.checkValue = null;
 
   }
 
@@ -16,46 +56,81 @@ class Quiz{
   }
 
   changeScore(){
-    score++;
+    this.score++;
   }
   
   getQuestions(){
     //something something TriviaAPI
   }
 
-  getUnaskedQuestions(){
-    const questions = TriviaAPI.getQuestions;
-    this.unasked.push(questions);
+  quizInitialize(){
+    try{
+      this.getUnaskedQuestions();
+      this.toggleActive();
+    } catch (e){
+      console.log(e);
     }
+  }
+
+  getUnaskedQuestions(){
+    return TriviaAPI.getQuestions()
+      .then( questions => {
+        questions.results.forEach(item=> {
+          this.unasked.push(new Question(item.question, item.incorrect_answers, item.correct_answer));
+        });
+      })
+      .then(()=>{
+        this.questionController();
+      });
+
+    //this.unasked.push(...questions);
+  }
+
 
   questionController(){
-    toggleActive()
     while (this.unasked.length !== 0){
-    askQuestion()
-    promptUser()
-    this.checkValue = checkAnswer(currentQuestion.userAnswer);
+      this.askQuestion();
+      this.promptUser();
     }
-    toggleActive()
+    this.toggleActive();
   }
 
   promptUser(){
-    currentQuestion.userAnswer = prompt('Your answer?' 0);
+    let chosenQuestion = this.asked[0];
+    //HARD CODED
+    chosenQuestion.userAnswer = chosenQuestion.incorrectAnswers[1];
+
+    this.logQuizResponse(chosenQuestion.answerStatus());
+    //console.log(this.asked[0])
   }  
   askQuestion(){
     //CHANGE FOR RENDER
     let currentQuestion = this.unasked.pop();
-    let answerChoices = [...currentQuestion.answers, currentQuestion.correctAnswer];
+
+    let answerChoices = [...currentQuestion.incorrectAnswers, currentQuestion.correctAnswer];
+
+    function shuffle(a) {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    }
+  
+    answerChoices = shuffle(answerChoices);
+
     console.log(currentQuestion.text);
     console.log(answerChoices);
-    
+
+    this.asked.push(currentQuestion);
   }
 
-  logQuizResponse(answer){
-    if (this.checkValue ===1){
-      console.log('You are Correct')
+  logQuizResponse(checkValue){
+    if (checkValue ===1){
+      console.log('You are Correct');
       changeScore();
-    } else if(this.checkValue ===0){
-      console.log('YOU ARE SO WRONG!')
+    } else if(checkValue ===0){
+      console.log('YOU ARE SO WRONG!');
     } else console.log('Skipped');
   }
 
@@ -90,50 +165,6 @@ class Question{
   }
 }
 
+let x = new Quiz();
 
-
-
-
-
-
-
-
-
-
-class TriviaApi {
-constructor(){
-  this.allQuestions = [];
-}
-
-  errorHandler(response) {
-    let error;
-    if (!response.ok) {
-      error.status = response.status;
-      if (response.headers.get('Content-Type').includes('json')) {
-        error.message = response.statusText;
-        return Promise.reject(error);
-      }
-    }
-    if (error) {
-      error.message = response.message;
-      return Promise.reject(error);
-    }
-    return response.json();
-  }
-
-  api() {
-    console.log('hello');
-    const BASE_URL = 'https://opentdb.com/api.php?amount=5';
-
-    // function getQuestion() {
-  
-  fetch(`${BASE_URL}`)
-      .then(this.errorHandler)
-      .then(res => res.results.forEach(question => {
-        this.allQuestions.push( new Question(question.question, question.incorrect_answers, question.correct_answer));
-      }))
-      .catch(e => console.log(e.message));
-    // }
-  }
-
-}
+x.quizInitialize();
